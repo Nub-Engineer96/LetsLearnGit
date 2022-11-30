8efd73a356d1785a77b981e297cbb54f4ccb17a3
Amitabha Mukhopadhyay <amitabha2021@gmail.com>
2022-11-30 19:18:11 +0530

    added message in PROG1.java

--- prog1.java
+++ prog1.java
@@ -2,7 +2,6 @@ class Sum{
     public static void main (String args[]){
         int a=20;
         int b= 30;
-        int sum = a+b;
-        System.out.println("The sum is " +sum);
+        System.out.println("The sum is " +(a+b));
     }
 }
